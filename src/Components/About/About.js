a6a94647a380f81caecf16135d9eb2412e93ef6f
Zayn Yusuf <yusue786@gmail.com>
2022-04-18 22:08:32 +0600

    add-about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,51 @@ import'./About.css'
 const About = () => {
     return (
         <div className='min-width'>
-            <h1>This is about this web site</h1>
+        <p>
+        We create artworks, concepts, photography & choreography
+Who We Are?
+Every person has moments that they want to remember forever. Such moment are not only captured, but used to create the most beautiful, artistic, and fulfilling concepts. They are immortalized into photographic artworks, choreography, and impressions that will turn heads for ever.
+
+At NijolCreative Photography we understand this rule that makes the difference. As the leading photography agency in Bangladesh, we have spent every single moments of our time creating and not capturing, building and not putting, designing and not setting, magical moments that have won over many hearts and left us with a powerful experience to make our every life we touch satisfied and happy.
+
+Abu Sufian Nilove the CEO and the rest of our team have shown high style dedication to the path of developing new ideas and learning new concept. Additionally, they have carved out a name for the “best professional photographer in the business.”
+
+What Makes Us Different?
+Our team of professionals at NijolCreative Photography listens to our customers. We create photography by making a suitable and safe environment that allows the client to express their inner self. We understand how to create and capture the right moment the right way.
+
+Our approach to customer satisfaction is based on listening to our clients and working through projects together as partners. This has allowed us to foster some of the best long term relationship with our client and assist them to capture and preserve the most meaningful moments of their lives.
+
+We have a passion for photography and everything that comes with it. Our team of expert photographers will tell your wedding stories every second across the clock in a manner that they will be cherished for years to come.
+
+We will design the most excellent crafts, arts, and graphics in Videography, Digital Branding, and Online Advertisement that will boost your success. Indeed at Nijol Creative we offer the best way to tell the story of your life and your brand.
+
+Today, our clients come from many places. From Bangladesh, India, all through to the US our impact has changed the world. We are offering professional photographic education intended to increase skilled and qualified photographers in the country.
+
+With our new branches opening their doors in India and the US, don’t be left behind. Choose Nijol Creative Photography and make the leap of satisfaction, joy, and success in telling a unique, original, and authentic story.
+
+Our joy is our passion to photography, shooting, and capturing moments and how they change your life and make all of us joyous and happy.
+
+Why NijolCreative?
+We do what we are best at. We photograph memories, create stories and show you the beauty you would not see otherwise.
+
+Professionally trained photographers
+Over 10 years combined experience.
+We create art and crafts with photography
+We offer 24/7 support and services
+We are affordable and budget friendly
+We keep our clients happy and satisfied.
+What We DO?
+We offer various photographic services. Here are some of the most fundamental ones that we will leave you with.
+
+Wedding Photography/ Videography
+Product Photography/ Videography
+Fashion Photography/ Videography
+Commercial Photography/ Videography
+Our Motto
+Let’s walk together, run after excellence, maintaining premium quality, and tell the story that lives forever, with photos!
+
+So, here we are, at your service, now and always.
+        </p>
         </div>
     );
 };
